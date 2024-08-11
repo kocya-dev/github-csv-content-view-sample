886f863e55c32f4fd7c7982cfe5dd10628129277
kohei.mochizuki <kohei.mochizuki@xacti-co.com>
2024-08-12 00:03:03 +0900

    env: test targets, etc.

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,16 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      globals: true,
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      coverage: {
+        enabled: true,
+        reporter: ['text', 'html'],
+        include: ['src/**/*']
+      }
     }
   })
 )
