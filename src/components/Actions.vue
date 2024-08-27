8e6c1c87b7c86bf9f8ce1aaf304e332835b543bd
kohei.mochizuki <kohei.mochizuki@xacti-co.com>
2024-08-27 23:52:17 +0900

    不要な画面を削除、最小限のコンポーネント追加

---
+++ src/components/Actions.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+    <el-tab-pane label="Weekly" name="Weekly">Weekly</el-tab-pane>
+    <el-tab-pane label="Monthly" name="Monthly"><MonthlyResult msg="test" /></el-tab-pane>
+    <el-tab-pane label="Quaterly" name="Quaterly">Quaterly</el-tab-pane>
+  </el-tabs>
+</template>
+
+<script lang="ts" setup>
+import MonthlyResult from '../components/MonthlyResult.vue'
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
+
+const activeName = ref('first')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
+</script>
+
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import LeftTable from 'element-plus'
+import Actions from './Actions.vue'
+
+import { ref } from 'vue'
+import type { TabsInstance } from 'element-plus'
+
+const tabPosition = ref<TabsInstance['tabPosition']>('left')
+</script>
+
+<template>
+  <div>
+    <el-tabs :tab-position="tabPosition" style="padding: 32px" class="demo-tabs">
+      <el-tab-pane label="User"><Actions /></el-tab-pane>
+      <el-tab-pane label="Config">Config</el-tab-pane>
+      <el-tab-pane label="Role">Role</el-tab-pane>
+      <el-tab-pane label="Task">Task</el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 8px 32px 8px 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+
+.el-tabs--right .el-tabs__content,
+.el-tabs--left .el-tabs__content {
+  height: 100%;
+}
+</style>
