cfde8b474311914641f6f44955d45856c324b020
kohei.mochizuki <kohei.mochizuki@xacti-co.com>
2024-09-01 10:19:35 +0900

    http csv取得で例外処理追加

--- src/model/utils/httpCsvGetter.ts
+++ src/model/utils/httpCsvGetter.ts
@@ -8,20 +8,26 @@ import { parse } from 'csv-parse/sync'
  */
 export const getHttpCsv = async (url: string) => {
   //https://raw.githubusercontent.com/kocya-dev/github-csv-content-view-sample/main/data/2024-08.csv,
-  const response = await axios.get(url, {
-    headers: {
-      'Content-type': 'text/csv'
-    }
-  })
-  /*
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'Content-type': 'text/csv'
+      }
+    })
+    /*
     .then((res: any) => console.log(res.data))
     .catch((error: any) => console.error(error))
   */
 
-  const records = parse(response.data, {
-    columns: true, // 全列を取得
-    skip_empty_lines: true, // 空行をスキップ
-    relax_column_count: true // 列数をチェックしない
-  })
-  return records
+    const records = parse(response.data, {
+      columns: true, // 全列を取得
+      skip_empty_lines: true, // 空行をスキップ
+      relax_column_count: true // 列数をチェックしない
+    })
+    return records
+  } catch (error) {
+    console.error(`get error: ${url}`)
+    console.error(error)
+    return []
+  }
 }
